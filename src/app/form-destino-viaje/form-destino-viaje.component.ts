8f07a236d3e1646af748470f64ecffd485ae7ad7
Dino Tomassini <dinotomassini@gmail.com>
2021-01-16 16:47:56 -0300

    Agregado los validadores leccion 3

--- src/app/form-destino-viaje/form-destino-viaje.component.ts
+++ src/app/form-destino-viaje/form-destino-viaje.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Output, EventEmitter } from "@angular/core";
-import { FormGroup, FormBuilder } from "@angular/forms";
+import { FormGroup, FormBuilder, Validators, FormControl, ValidatorFn } from "@angular/forms";
 import { DestinoViaje } from "../models/destino-viaje.model";
 
 @Component({
@@ -10,14 +10,19 @@ import { DestinoViaje } from "../models/destino-viaje.model";
 export class FormDestinoViajeComponent implements OnInit {
   @Output() onItemAdded: EventEmitter<DestinoViaje>;
   fg: FormGroup;
+  minLongitud: number = 5;
 
   constructor(private fb: FormBuilder) {
     //inicializar
     this.onItemAdded = new EventEmitter();
     //vinculacion con un tag html
     this.fg = this.fb.group({
-      lugar: [""],
-      urlImage: [""],
+      lugar: ["", Validators.compose([
+        Validators.required,
+        // this.lugarValidator,
+        this.lugarValidatorParamtrizable(this.minLongitud)
+      ])],
+      urlImage: ["", Validators.required],
     });
 
     //observer de tipeo
@@ -32,5 +37,24 @@ export class FormDestinoViajeComponent implements OnInit {
     const destino = new DestinoViaje(lugar, urlImage);
     this.onItemAdded.emit(destino);
     return false;
+  }
+
+  lugarValidator(control: FormControl): { [s:string]: boolean } {
+    const l = control.value.toString().trim().length;
+    if (l > 0 && l < 5) {
+      return { invalidLugar: true};
+    } else {
+      return null;
+    }
+  }
+
+  lugarValidatorParamtrizable(minLong: number): ValidatorFn {
+    return (control: FormControl): { [s: string]: boolean } | null => {
+      const l = control.value.toString().trim().length;
+      if (l > 0 && l < minLong) {
+        return { minLongLugar: true };
+      }
+      return null;
+    }
   }
 }
